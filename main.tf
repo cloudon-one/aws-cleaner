b685c60051ae5b49d739529616a6e1ee914efab2
Daniel Vaknin <daniel@terasky.com>
2021-07-15 18:38:46 +0300

    Add variables and policy

--- main.tf
+++ main.tf
@@ -7,15 +7,15 @@ module "lambda_function" {
   version = "2.7.0"
 
   function_name = var.function_name
-  description   = "My awesome lambda function"
+  description   = var.function_description
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
   publish       = true
 
   source_path = "files/index.py"
 
-  # create_package         = false
-  # local_existing_package = "${path.module}/../fixtures/python3.8-zip/existing_package.zip"
+  attach_policy = true
+  policy        = "arn:aws:iam::aws:policy/AdministratorAccess"
 
   allowed_triggers = {
     NightlyRule = {
@@ -31,7 +31,7 @@ module "lambda_function" {
 resource "aws_cloudwatch_event_rule" "nightly" {
   name                = "RunNightly"
   description         = "Stops all EC2 instances every night at 10 PM (8 PM GMT)"
-  schedule_expression = "cron(0 20 * * ? *)"
+  schedule_expression = var.event_cron
 }
 
 resource "aws_cloudwatch_event_target" "nightly_cleanup_lambda_function" {
--- variables.tf
+++ variables.tf
@@ -8,7 +8,9 @@ variable "default_tags" {
   type        = map(string)
   description = "Tags to apply across all resources handled by this provider"
   default = {
-    Terraform = "True"
+    Terraform       = "True"
+    Terraform_Cloud = "True"
+    Owner           = "Daniel Vaknin"
   }
 }
 
@@ -16,4 +18,16 @@ variable "function_name" {
   type        = string
   description = "Name of the Lambda function"
   default     = "NightlyClean"
+}
+
+variable "function_description" {
+  type        = string
+  description = "Description of the Lambda function"
+  default     = "Lambda function to cleanup unneeded resources (unattached EBS volumes, unattached EIPs, etc.)"
+}
+
+variable "event_cron" {
+  type        = string
+  description = "Cron value for the EventBridge rule"
+  default     = "cron(0 20 * * ? *)"
 }
